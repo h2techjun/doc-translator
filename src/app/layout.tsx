8ad5cb1bc34a50b2aa3f1c86d918708d1e2c3429
Deploy User <deploy@doctranslator.com>
2026-01-31 03:42:14 +0900

    feat: integrate ad block recovery scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,11 @@ export default function RootLayout({
       <head>
         <meta name="google" content="notranslate" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8134930906845147" crossOrigin="anonymous"></script>
+        {/* Ad Block Recovery Scripts */}
+        <script async src="https://fundingchoicesmessages.google.com/i/pub-8134930906845147?ers=1"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();`
+        }} />
       </head>
       <body className={inter.className}>
         <ThemeProvider
